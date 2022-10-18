f01cb1a29ba108744ea53301566d0d5762db2430
정혜주 <fixup719@gmail.com>
2022-10-19 00:38:04 +0900

    스프링수업 코드

---
+++ Simple_MVC/src/main/java/com/ssafy/mvc/model/service/BookServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ssafy.mvc.model.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.mvc.dto.Book;
+import com.ssafy.mvc.model.dao.BookDao;
+
+// 빈 등록
+@Service
+public class BookServiceImpl implements BookService {
+	
+	//BookDao 객체 주입
+	@Autowired
+	BookDao bookDao;
+
+	@Override
+	public List<Book> selectAll() throws SQLException {
+		return bookDao.selectAll();
+	}
+	
+	
+}
